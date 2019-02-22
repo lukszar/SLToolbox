1219539b3b8174615687ef65e8d8e04a0148a73e
Łukasz Szarkowicz <szarkowicz@gmail.com>
2019-02-22 14:51:55 +0100

    Reordering catalogs

---
+++ Sources/SLToolbox.h
@@ -0,0 +1,19 @@
+//
+//  SLToolbox.h
+//  SLToolbox
+//
+//  Created by Lukasz Szarkowicz on 22/02/2019.
+//  Copyright © 2019 Łukasz Szarkowicz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SLToolbox.
+FOUNDATION_EXPORT double SLToolboxVersionNumber;
+
+//! Project version string for SLToolbox.
+FOUNDATION_EXPORT const unsigned char SLToolboxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SLToolbox/PublicHeader.h>
+
+
