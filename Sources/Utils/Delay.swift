c4b69e343d86d8d45635eb773092cd9ee2f310eb
Łukasz Szarkowicz <szarkowicz@gmail.com>
2019-04-24 11:43:43 +0200

    Delay function

--- Sources/Utils/Delay.swift
+++ Sources/Utils/Delay.swift
@@ -9,7 +9,16 @@
 import Foundation
 
 class Delay {
-    
+
+    /**
+     Execute block of code with delay.
+
+     - parameter delay: value in seconds after which completion block will be executed
+
+     - parameter completion: code block to execute after provided delay time
+
+     - Author: SLToolbox - Łukasz Szarkowicz
+     */
     class func after(_ delay: Double, completion:@escaping () -> Void) {
         DispatchQueue.main.asyncAfter(
             deadline: DispatchTime.now() + Double(Int64(delay * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC),
