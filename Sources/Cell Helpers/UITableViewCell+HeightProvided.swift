d7a2387f4ad4ef06ca1a4a45c6a5f0bfc4a3413f
Łukasz Szarkowicz <szarkowicz@gmail.com>
2019-06-04 11:32:33 +0200

    Protocol for cell height provided

---
+++ Sources/Cell Helpers/UITableViewCell+HeightProvided.swift
@@ -0,0 +1,15 @@
+//
+//  UITableViewCell+HeightProvided.swift
+//  SLToolbox
+//
+//  Created by Lukasz Szarkowicz on 10/05/2019.
+//  Copyright © 2019 Łukasz Szarkowicz. All rights reserved.
+//
+
+import Foundation
+
+public protocol HeightProvided: UITableViewCell {
+
+    static var providedHeight: CGFloat { get }
+    static var providedEstimatedHeight: CGFloat { get }
+}
